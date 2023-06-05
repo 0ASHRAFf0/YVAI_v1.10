d41d7e8779d7686654bea526f2907c1f6135f1e2
Mohamed Ashraf <mohamedashrafaly2018@gmail.com>
2023-06-06 02:19:44 +0300

    add additional about options.txt

--- src/options.py
+++ src/options.py
@@ -1,5 +1,24 @@
+import os
+import datetime
+
+
 class Options():
     def __init__(self, videoDir_Op, audioDir_Op, thumbDir_Op):
         self.video_directory = videoDir_Op
         self.audio_directory = audioDir_Op
         self.thumbnail_directory = thumbDir_Op
+
+    @staticmethod
+    def write_options():
+        with open('options.txt', 'w+') as Op_file:
+            Op_file.write(
+                f'{datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S")} "{os.path.dirname(os.path.realpath(__file__))}"\n')
+
+    @staticmethod
+    def read_options():
+        with open('options.txt', 'r') as Op_file:
+            print(Op_file.readline())
+
+
+Options.write_options()
+Options.read_options()
